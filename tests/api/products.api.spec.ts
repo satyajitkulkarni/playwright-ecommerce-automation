4a3281367685553025d8f7ac6bc17ad801f75e4e
satyajitkulkarni <satyajit.v.kulkarni@gmail.com>
2026-09-16 20:01:26 +0530

    Add smoke and regression test tagging

--- tests/api/products.api.spec.ts
+++ tests/api/products.api.spec.ts
@@ -4,99 +4,114 @@ import { Product, } from '../../src/types/apiTypes';
 
 test.describe('Products API', () => {
 
-    test('should retrieve products', async ({ request }) => {
+    test('should retrieve products',
+        {
+            tag: '@smoke',
+        }, async ({ request }) => {
 
-        const productsApi = new ProductsApi(request);
+            const productsApi = new ProductsApi(request);
 
-        const response =
-            await productsApi.getProducts();
+            const response =
+                await productsApi.getProducts();
 
-        expect(response.ok()).toBeTruthy();
-        expect(response.status()).toBe(200);
+            expect(response.ok()).toBeTruthy();
+            expect(response.status()).toBe(200);
 
-        const products = await response.json();
+            const products = await response.json();
 
-        expect(Array.isArray(products)).toBeTruthy();
-        expect(products.length).toBeGreaterThan(0);
-    });
-
-
-    test('should create a product', async ({ request }) => {
-
-        const productsApi = new ProductsApi(request);
-
-        const product: Product = {
-            title: 'Playwright Automation Product',
-            body: 'Created by API automation',
-            userId: 1,
-        };
-
-        const response =
-            await productsApi.createProduct(product);
-
-        expect(response.status()).toBe(201);
-
-        const responseBody = await response.json();
-
-        expect(responseBody.title).toBe(product.title);
-        expect(responseBody.body).toBe(product.body);
-        expect(responseBody.userId).toBe(product.userId);
-    });
+            expect(Array.isArray(products)).toBeTruthy();
+            expect(products.length).toBeGreaterThan(0);
+        });
 
 
-    test('should update a product', async ({ request }) => {
+    test('should create a product',
+        {
+            tag: '@smoke',
+        }, async ({ request }) => {
 
-        const productsApi = new ProductsApi(request);
+            const productsApi = new ProductsApi(request);
 
-        const updatedProduct: Product = {
-            id: 1,
-            title: 'Updated Automation Product',
-            body: 'Updated using API automation',
-            userId: 1,
-        };
+            const product: Product = {
+                title: 'Playwright Automation Product',
+                body: 'Created by API automation',
+                userId: 1,
+            };
 
-        const response =
-            await productsApi.updateProduct(
-                1,
-                updatedProduct,
-            );
+            const response =
+                await productsApi.createProduct(product);
 
-        expect(response.status()).toBe(200);
+            expect(response.status()).toBe(201);
 
-        const responseBody = await response.json();
+            const responseBody = await response.json();
 
-        expect(responseBody.title)
-            .toBe(updatedProduct.title);
-
-        expect(responseBody.body)
-            .toBe(updatedProduct.body);
-    });
+            expect(responseBody.title).toBe(product.title);
+            expect(responseBody.body).toBe(product.body);
+            expect(responseBody.userId).toBe(product.userId);
+        });
 
 
-    test('should delete a product', async ({ request }) => {
+    test('should update a product',
+        {
+            tag: '@regression',
+        }, async ({ request }) => {
 
-        const productsApi = new ProductsApi(request);
+            const productsApi = new ProductsApi(request);
 
-        const response =
-            await productsApi.deleteProduct(1);
+            const updatedProduct: Product = {
+                id: 1,
+                title: 'Updated Automation Product',
+                body: 'Updated using API automation',
+                userId: 1,
+            };
 
-        expect(response.status()).toBe(200);
-    });
+            const response =
+                await productsApi.updateProduct(
+                    1,
+                    updatedProduct,
+                );
 
-    test('should partially update a product', async ({ request }) => {
-        const productsApi = new ProductsApi(request);
+            expect(response.status()).toBe(200);
 
-        const patchData = {
-            title: 'Partially Updated Product',
-        };
+            const responseBody = await response.json();
 
-        const response = await productsApi.patchProduct(1, patchData);
+            expect(responseBody.title)
+                .toBe(updatedProduct.title);
 
-        expect(response.status()).toBe(200);
+            expect(responseBody.body)
+                .toBe(updatedProduct.body);
+        });
 
-        const responseBody = await response.json();
 
-        expect(responseBody.title).toBe(patchData.title);
-    });
+    test('should delete a product',
+        {
+            tag: '@regression',
+        }, async ({ request }) => {
+
+            const productsApi = new ProductsApi(request);
+
+            const response =
+                await productsApi.deleteProduct(1);
+
+            expect(response.status()).toBe(200);
+        });
+
+    test('should partially update a product',
+        {
+            tag: '@regression',
+        }, async ({ request }) => {
+            const productsApi = new ProductsApi(request);
+
+            const patchData = {
+                title: 'Partially Updated Product',
+            };
+
+            const response = await productsApi.patchProduct(1, patchData);
+
+            expect(response.status()).toBe(200);
+
+            const responseBody = await response.json();
+
+            expect(responseBody.title).toBe(patchData.title);
+        });
 
 });
